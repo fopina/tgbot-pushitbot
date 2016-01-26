6822394dec420708b586ea9cc5278b5ec29bc07d
Filipe Pina <fopina@gmail.com>
2016-01-26 08:55:14 +0000

    token

--- pushitbot.py
+++ pushitbot.py
@@ -4,23 +4,69 @@
 from tgbot import TGBot
 from tgbot.pluginbase import TGPluginBase, TGCommandBase
 import argparse
+import os
 
 
 class PushItPlugin(TGPluginBase):
     def list_commands(self):
         return (
-            TGCommandBase('token', self.token, 'Get your API token'),
+            TGCommandBase('token', self.token, 'view your API token'),
+            TGCommandBase('revoke', self.revoke, 'revoke your API token and create a new one'),
+            TGCommandBase('help', self.help, 'more information'),
         )
 
     def token(self, message, text):
-        return self.bot.return_message(message.chat.id, 'done')
+        token = self.read_data(message.chat.id, 'token')
+        if not token:
+            token = self._new_token(message.chat.id)
+
+        return self.bot.return_message(message.chat.id, '''\
+You can use the following token to access the HTTP API:
+
+*%s*
+
+Please send /help command if you have any problem''' % token, parse_mode='Markdown')
+
+    def revoke(self, message, text):
+        token = self._new_token(message.chat.id)
+
+        return self.bot.return_message(message.chat.id, '''\
+Your _old_ token has been revoked.
+You can now use the following token to access the HTTP API:
+
+*%s*
+
+Please send /help command if you have any problem''' % token, parse_mode='Markdown')
+
+    def help(self, message, text):
+        return self.bot.return_message(message.chat.id, u'''\
+I'm PushIt Bot and I can help you integrate your scripts or website with Telegram.
+Please read this manual before we begin:
+
+ 📖 http://fopina.github.com/tgbot-pushitbot/api-docs
+
+Here is the commands list:
+
+/token - view your API token
+/revoke - revoke your API token and create a new one
+''', parse_mode='Markdown')
+
+    def chat(self, message, text):
+        return self.bot.return_message(message.chat.id, u'''
+I'm not really chatty. Give /help a try if you need something.
+''')
+
+    def _new_token(self, chat_id):
+        token = os.urandom(16).encode('hex')
+        self.save_data(chat_id, 'token', token)
+        return token
 
 
 def setup(db_url=None, token=None):
+    p = PushItPlugin()
     tg = TGBot(
-        token,
-        plugins=[PushItPlugin()],
-        db_url=db_url,
+        token, plugins=[p],
+        no_command=p, db_url=db_url,
     )
     return tg
 
